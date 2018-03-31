8ca9d0b025cf717dbf71cc6dc18a859806e3eeaf
leonardas103 <wingdragon_9@hotmail.com>
2018-03-31 21:35:00 +0200

    Update utctime.c

--- utctime.c
+++ utctime.c
@@ -1,10 +1,30 @@
+/*
+utctime.c
+*/
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/cdefs.h>
+#include "namespace.h"
+#include <lib.h>
 #include <time.h>
 
-int main (void) {
-    time_t now = time(NULL);
-    time_t utc = utctime(NULL);
-    printf("UNX:\t %ld\nUTC:\t %ld\nDIFF:\t %ld\n", (long)now, (long)utc, (long)(utc-now));
-    return 0;
+time_t utctime (time_t *tp) {
+    message m;
+
+    // Perform system call.
+    if (_syscall(PM_PROC_NR, UTCTIME, &m) < 0) {
+        fprintf(stderr, "Error: do_utctime failed!\n");
+        return -1;
+    }
+
+    // Extract time (m2_l1 is a long, so recast).
+    time_t utime = (time_t)m.m2_l1;
+
+    // If pointer is non-null, save it there too. 
+    if (tp != NULL) {
+        *tp = utime;
+    }
+
+    // Return result.
+    return utime;
 }
