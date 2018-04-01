7c9b369c6d51af179313f9ed5b12a287667f5ac2
leonardas103 <wingdragon_9@hotmail.com>
2018-04-01 23:14:27 +0200

    Create copy.sh

---
+++ copy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cp main.c /usr/src/kernel/
+cp table.c /usr/src/servers/pm/
+cp callnr.h /usr/src/include/minix/
+cp proto.h /usr/src/servers/pm/
+cp do_utctime.c /usr/src/servers/pm/
+cp Makefile /usr/src/servers/pm/
+cp utctime.c /usr/src/lib/libc/sys-minix/
+cp Makefile.inc /usr/src/lib/libc/sys-minix/
+cp time.h /usr/src/include/
